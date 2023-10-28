bcb3e6bc914d0877b88e579ca44cd0134073079b
Hanzanka <ville.einio@gmail.com>
2023-10-28 21:05:44 +0300

    feat: define fonts in separate file

---
+++ src/ui/fonts.py
@@ -0,0 +1,5 @@
+from customtkinter import CTkFont
+
+title_font = CTkFont(family="Fira Code", weight="bold", size=30)
+update_panel_font = CTkFont(family="Fira Code", weight="bold", size=12)
+side_panel_font = CTkFont(family="Fira Code", weight="bold", size=14)

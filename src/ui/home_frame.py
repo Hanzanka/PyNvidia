c8ceafb6deebe32386f8b8ee520ca73120007b80
Hanzanka <ville.einio@gmail.com>
2023-11-17 22:23:05 +0200

    feat: monitorframe

--- src/ui/home_frame.py
+++ src/ui/home_frame.py
@@ -3,28 +3,30 @@ import tkinter
 from gpu.gpu import GPU
 from ui.update_frame import UpdateFrame
 from scraper.scraper import NvidiaWebScraper
+from ui.fonts import title_font
+from ui.monitor_frame import MonitorFrame
 
 
 class HomeFrame(customtkinter.CTkFrame):
     def __init__(self, master, driver_downloader: NvidiaWebScraper) -> None:
         super().__init__(master=master, corner_radius=5, fg_color="#0b0b0b")
 
-        self.pack(
-            fill=tkinter.BOTH, padx=(0, 10), pady=10, expand=True, side=tkinter.RIGHT
-        )
-
-        title_font = customtkinter.CTkFont(family="Fira Code", weight="bold", size=30)
-
         gpu = GPU()
 
         title = customtkinter.CTkLabel(
             master=self,
-            text=f"NVidia {gpu.product} {gpu.vram}",
+            text=gpu.full_name,
             font=title_font,
             padx=10,
             pady=10,
             corner_radius=5,
         )
-
         title.pack()
-        UpdateFrame(master=self, driver_downloader=driver_downloader)
+
+        self.__update_frame = UpdateFrame(
+            master=self, driver_downloader=driver_downloader
+        )
+        self.__update_frame.pack(fill=tkinter.X, side=tkinter.BOTTOM, padx=10, pady=(5, 10))
+
+        self.__monitor_frame = MonitorFrame(master=self, gpu=gpu)
+        self.__monitor_frame.pack(fill=tkinter.BOTH, side=tkinter.BOTTOM, padx=10)

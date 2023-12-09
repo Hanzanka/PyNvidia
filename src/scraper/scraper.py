bd8b37fb9ca5698652dff14f84a6165d3fffdd45
Hanzanka <ville.einio@gmail.com>
2023-12-10 00:19:32 +0200

    perfomance: Switch selenium to Beautifulsoup and urllib

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,86 +1,107 @@
-from selenium.webdriver import Edge
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from urllib.request import urlretrieve
-from gpu.gpu import GPU
+from urllib.request import urlretrieve, urlopen
+from src.gpu.gpu import GPU
 import os
-from utils.utils import get_path
+from src.utils.utils import get_path
+from PyQt6.QtCore import pyqtSignal, pyqtSlot, QObject
+from threading import Lock, Timer
+from src.utils.utils import delete_unfinished_download, get_nv_search_keys
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 
-class NvidiaWebScraper:
+class NvidiaWebScraper(QObject):
+    check_for_update_signal = pyqtSignal(bool, float)
+    check_for_update_error_signal = pyqtSignal()
+
+    download_signal = pyqtSignal(int, float, float, float)
+    download_error_signal = pyqtSignal()
+    download_complete_signal = pyqtSignal(bool)
+
     def __init__(self, gpu: GPU) -> None:
-        options = Options()
-        options.headless = True
-        options.add_argument("--blink-settings=imagesEnabled=false")
-        self.__webdriver = Edge(options=options)
-        self.__search_page = "https://www.nvidia.com/download/index.aspx"
+        super().__init__()
         self.__gpu = gpu
         self.__download_path = get_path("download")
-        
-    def download_installer(self, reporthook=None) -> None:
-        self.__open_search_page()
-        link = self.__get_download_link()
-        if not os.path.isdir(self.__download_path):
-            os.makedirs(self.__download_path)
-        download_location = self.__download_path + f"{link.split('/')[4]}.exe"
-        urlretrieve(url=link, filename=download_location, reporthook=reporthook)
+        self.__available_driver_version = None
+        self.__report_lock = Lock()
+        self.__last_update = datetime.now()
+        self.__last_size = 0
 
-    def check_for_updates(self) -> tuple:
-        self.__open_search_page()
-        driver_version = float(self.__webdriver.find_element(
-            by=By.XPATH, value='//*[@id="tdVersion"]'
-        ).text.split("  ")[0])
-        return self.__gpu.driver_version != driver_version, driver_version
+    def __get_driver_url(self) -> str:
+        keys = get_nv_search_keys(
+            product_series=self.__gpu.series, product=self.__gpu.product
+        )
+        url = (
+            "processDriver.aspx"
+            + f"?psid={keys[0]}"
+            + f"&pfid={keys[1]}"
+            + "&rpf=1"
+            + "&osid=57"
+            + "&lid=1"
+            + "&lang=en-us"
+            + "&ctk=0"
+            + "&dtid=1"
+            + "&dtcid=1"
+        )
+        return f"https://www.nvidia.com/download/{urlopen(f'https://www.nvidia.com/download/{url}').read().decode()}"
 
-    def __open_search_page(self) -> None:
-        self.__webdriver.get(self.__search_page)
-
-        Select(
-            self.__webdriver.find_element(by=By.ID, value="selProductSeriesType")
-        ).select_by_visible_text(text=self.__gpu.type)
-
-        Select(
-            webelement=self.__webdriver.find_element(by=By.ID, value="selProductSeries")
-        ).select_by_visible_text(text=self.__gpu.series)
-
-        Select(
-            webelement=self.__webdriver.find_element(by=By.ID, value="selProductFamily")
-        ).select_by_visible_text(text=self.__gpu.product)
-
-        Select(
-            webelement=self.__webdriver.find_element(
-                by=By.ID, value="selOperatingSystem"
-            )
-        ).select_by_visible_text(text="Windows 10 64-bit")
-
-        Select(
-            webelement=self.__webdriver.find_element(
-                by=By.ID, value="ddlDownloadTypeCrdGrd"
-            )
-        ).select_by_visible_text(text="Game Ready Driver (GRD)")
-
-        Select(
-            webelement=self.__webdriver.find_element(by=By.ID, value="ddlLanguage")
-        ).select_by_visible_text(text="English (US)")
-
-        self.__webdriver.execute_script("javascript: GetDriver();")
-
-    def __get_download_link(self) -> str:
-        self.__webdriver.get(
-            self.__webdriver.find_element(
-                by=By.XPATH, value='//*[@id="lnkDwnldBtn"]'
-            ).get_attribute(name="href")
+    @pyqtSlot()
+    def check_for_updates(self) -> None:
+        try:
+            page = urlopen(self.__get_driver_url()).read().decode()
+        except Exception:
+            self.check_for_update_error_signal.emit()
+            return
+        soup = BeautifulSoup(page, "html.parser")
+        td = soup.find("td", {"id": "tdVersion"})
+        self.__available_driver_version = float(td.text.split()[0])
+        self.check_for_update_signal.emit(
+            self.__gpu.driver_version != self.__available_driver_version,
+            self.__available_driver_version,
         )
 
-        return self.__webdriver.find_element(
-            by=By.XPATH, value='//*[@id="mainContent"]/table/tbody/tr/td/a'
-        ).get_attribute(name="href")
+    @pyqtSlot()
+    def download_driver(self) -> None:
+        try:
+            if not os.path.isdir(self.__download_path):
+                os.makedirs(self.__download_path)
+            download_location = (
+                self.__download_path + f"{self.__available_driver_version}.exe"
+            )
+            urlretrieve(
+                url="https://us.download.nvidia.com/Windows/"
+                + str(self.__available_driver_version)
+                + "/"
+                + str(self.__available_driver_version)
+                + "-desktop-win10-win11-64bit-international-dch-whql.exe",
+                filename=download_location,
+                reporthook=self.__update_download_progress,
+            )
+        except Exception:
+            self.download_error_signal.emit()
+            delete_unfinished_download(driver_version=self.__available_driver_version)
+            return
+        self.download_complete_signal.emit(None)
 
-    def quit(self) -> None:
-        self.__webdriver.quit()
-    
+    def __update_download_progress(self, block_num, block_size, total_size) -> None:
+        if self.__report_lock.acquire(blocking=False):
+            time_now = datetime.now()
+            size_now = block_num * block_size
+            self.download_signal.emit(
+                int(block_num * block_size / total_size * 100),
+                block_num * block_size / 1024**2,
+                total_size / 1024**2,
+                (size_now - self.__last_size)
+                / (time_now - self.__last_update).total_seconds()
+                / 1024**2,
+            )
+            self.__last_update = time_now
+            self.__last_size = size_now
+            Timer(interval=0.5, function=self.__report_lock.release).start()
+
+        if block_num * block_size >= total_size:
+            self.download_complete_signal.emit(True)
+
 
 if __name__ == "__main__":
-    gpu = GPU()
-    NvidiaWebScraper(gpu=gpu).download_installer()
+    gpu = GPU(laptop_dev=True)
+    scraper = NvidiaWebScraper(gpu=gpu)

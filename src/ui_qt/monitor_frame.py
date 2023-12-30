32ab339a32d0f60f96c04273d07c9f1385859b7b
Hanzanka <ville.einio@gmail.com>
2023-12-30 22:13:41 +0200

    Add gpu stat displayment

--- src/ui_qt/monitor_frame.py
+++ src/ui_qt/monitor_frame.py
@@ -5,12 +5,23 @@ from PyQt6.QtWidgets import (
     QProgressBar,
     QLabel,
 )
-from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt, QTimer, QThread, pyqtSignal
+from PyQt6.QtGui import QFont, QCursor
 from src.gpu.gpu import GPU
 
 
 class MonitorFrame(QFrame):
+    class StatUpdater(QThread):
+        def __init__(self, frames: list, gpu: GPU) -> None:
+            super().__init__()
+            self.__gpu = gpu
+            self.__frames = frames
+        
+        def run(self) -> None:
+            data = self.__gpu.get_sensor_data()
+            for frame in self.__frames:
+                frame.update_signal.emit(data[frame.data_property_id])
+    
     def __init__(self, gpu: GPU) -> None:
         super().__init__()
 
@@ -26,13 +37,13 @@ class MonitorFrame(QFrame):
         self.__layout.setContentsMargins(0, 0, 0, 0)
         self.__layout.setSpacing(7)
 
-        self.__items = []
+        self.__frames = []
 
         self.__util = MonitorPropertyFrame(
             title="Utilization", max_value=100, unit="%", data_property_id="utilization"
         )
         self.__layout.addWidget(self.__util, 0, 0, 1, 2)
-        self.__items.append(self.__util)
+        self.__frames.append(self.__util)
 
         self.__temp = MonitorPropertyFrame(
             title="Temperature",
@@ -41,7 +52,7 @@ class MonitorFrame(QFrame):
             data_property_id="temperature",
         )
         self.__layout.addWidget(self.__temp, 1, 0, 1, 2)
-        self.__items.append(self.__temp)
+        self.__frames.append(self.__temp)
 
         self.__clock = MonitorPropertyFrame(
             title="Core Clock",
@@ -50,19 +61,19 @@ class MonitorFrame(QFrame):
             data_property_id="clock",
         )
         self.__layout.addWidget(self.__clock, 2, 0)
-        self.__items.append(self.__clock)
+        self.__frames.append(self.__clock)
 
         self.__vram = MonitorPropertyFrame(
             title="VRAM", max_value=self.__gpu.vram_gb, unit="GB", data_property_id="vram_gb"
         )
         self.__layout.addWidget(self.__vram, 2, 1)
-        self.__items.append(self.__vram)
+        self.__frames.append(self.__vram)
 
         self.__fan = MonitorPropertyFrame(
             title="Fan Speed", max_value=100, unit="%", data_property_id="fan"
         )
         self.__layout.addWidget(self.__fan, 3, 0)
-        self.__items.append(self.__fan)
+        self.__frames.append(self.__fan)
 
         self.__power = MonitorPropertyFrame(
             title="Power",
@@ -71,21 +82,24 @@ class MonitorFrame(QFrame):
             data_property_id="power",
         )
         self.__layout.addWidget(self.__power, 3, 1)
-        self.__items.append(self.__power)
+        self.__frames.append(self.__power)
 
         self.setLayout(self.__layout)
 
-        self.__timer = QTimer()
-        self.__timer.timeout.connect(self.__update_data)
-        self.__timer.start(1000)
+        self.__updater = MonitorFrame.StatUpdater(frames=self.__frames, gpu=self.__gpu)
 
-    def __update_data(self) -> None:
-        data = self.__gpu.get_sensor_data()
-        for item in self.__items:
-            item.update_value(data[item.data_property_id])
+        self.__timer = QTimer()
+        self.__timer.timeout.connect(self.__updater.start)
+        self.__timer.setInterval(1000)
+        self.__timer.start()
+
+    def set_update_rate(self, msec: int) -> None:
+        self.__timer.setInterval(msec)
 
 
 class MonitorPropertyFrame(QFrame):
+    update_signal = pyqtSignal(object)
+    
     def __init__(
         self, title: str, max_value: int | float, unit: str, data_property_id: str
     ) -> None:
@@ -93,6 +107,8 @@ class MonitorPropertyFrame(QFrame):
 
         self.setStyleSheet("background-color: #121212; border-radius: 5px")
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        self.mouseReleaseEvent = lambda x: print("ok")
 
         font = QFont("Fira Code", 8, QFont.Weight.Bold)
 
@@ -111,7 +127,7 @@ class MonitorPropertyFrame(QFrame):
 
         self.__value = QLabel(f"0{self.__unit}")
         self.__value.setFont(font)
-        self.__value.setFixedWidth(55)
+        self.__value.setFixedWidth(50)
         self.__value.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.__layout.addWidget(self.__value, 1, 0, Qt.AlignmentFlag.AlignCenter)
 
@@ -120,24 +136,26 @@ class MonitorPropertyFrame(QFrame):
         self.__progress_bar.setMinimum(0)
         self.__progress_bar.setMaximum(max_value if type(max_value) is int else int(max_value * 10))
         self.__progress_bar.setStyleSheet(
-            "QProgressBar {border-radius: 5px; background-color: #0b0b0b; color: transparent}"
-            + " QProgressBar::chunk {background-color: #76b900; border-radius: 5px;}"
+            "QProgressBar {border-radius: 2px; background-color: #0b0b0b; color: transparent}"
+            + " QProgressBar::chunk {background-color: #76b900; border-radius: 2px;}"
         )
         self.__progress_bar.setValue(100)
         self.__layout.addWidget(self.__progress_bar, 1, 1)
 
         self.__max = QLabel(f"{max_value}{self.__unit}")
         self.__max.setFont(font)
-        self.__max.setFixedWidth(55)
+        self.__max.setFixedWidth(50)
         self.__max.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.__layout.addWidget(self.__max, 1, 2, Qt.AlignmentFlag.AlignCenter)
 
         self.setLayout(self.__layout)
+        
+        self.update_signal.connect(self.__update_value)
 
     @property
     def data_property_id(self) -> str:
         return self.__data_property_id
 
-    def update_value(self, value) -> None:
+    def __update_value(self, value) -> None:
         self.__value.setText(f"{value}{self.__unit}")
         self.__progress_bar.setValue(value if type(value) is int else int(value * 10))

64bb3b12cf6e5de6a3d3dbf3a2ae60ad596e8759
Hanzanka <ville.einio@gmail.com>
2023-12-30 22:11:26 +0200

    Declare the app instance as class instead of qframe

--- src/ui_qt/app.py
+++ src/ui_qt/app.py
@@ -1,30 +1,51 @@
-from PyQt6.QtWidgets import QApplication, QHBoxLayout, QWidget
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QWidget, QFrame
+from PyQt6.QtGui import QIcon, QPalette, QColor
 from src.ui_qt.side_panel import SidePanel
 from src.ui_qt.home_frame import HomeFrame
 from src.utils.utils import get_path
+from src.ui_qt.gpu_chart_frame import GPUChartFrame
 
 
-class App(QWidget):
+class App(QApplication):
     def __init__(self, argv) -> None:
-        self.__app = QApplication(argv)
-        super().__init__()
-
-        self.setGeometry(0, 0, 900, 500)
-        self.setMinimumSize(900, 500)
-        self.setWindowTitle("PyNvidia")
-        self.setWindowIcon(QIcon(get_path("icon")))
-        self.setStyleSheet("background-color: #050505;")
+        super().__init__(argv)
+        
+        self.__palette = QPalette()
+        self.__palette.setColor(QPalette.ColorRole.WindowText, QColor(255, 255, 255))
+        self.setPalette(self.__palette)
+        
+        self.__top_frame = QFrame()
+        self.__top_frame.setGeometry(0, 0, 900, 500)
+        self.__top_frame.setMinimumSize(900, 500)
+        self.__top_frame.setWindowTitle("PyNvidia")
+        self.__top_frame.setWindowIcon(QIcon(get_path("icon")))
+        self.__top_frame.setStyleSheet("background-color: #050505;")
 
         self.__side_panel = SidePanel()
         self.__home_frame = HomeFrame()
+        
+        self.__test = GPUChartFrame()
+        
+        self.__main_frame = None
+        self.__main_return_to = None
 
         self.__layout = QHBoxLayout()
-        self.__layout.addWidget(self.__side_panel)
-        self.__layout.addWidget(self.__home_frame)
         self.__layout.setContentsMargins(7, 7, 7, 7)
         self.__layout.setSpacing(7)
-        self.setLayout(self.__layout)
+        
+        self.__layout.addWidget(self.__home_frame)
+        self.set_main_frame(self.__test)
+        
+        self.__top_frame.setLayout(self.__layout)
+        
+        self.__top_frame.show()
+        
+        self.exec()
 
-        self.show()
-        self.__app.exec()
+    def set_main_frame(self, frame: QFrame) -> None:
+        if self.__main_frame is not None:
+            self.__main_return_to = self.__main_frame
+            self.__layout.removeWidget(self.__main_frame)
+            self.__main_frame.setParent(None)
+        self.__main_frame = frame
+        self.__layout.addWidget(self.__main_frame)

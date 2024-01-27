3b4fb04ea35d92ecc3968b10676780ebcab51fa0
Hanzanka <ville.einio@gmail.com>
2024-01-27 23:18:32 +0200

    Remove old ui

--- src/ui/fonts.py
+++
@@ -1,5 +0,0 @@
-from customtkinter import CTkFont
-
-title_font = CTkFont(family="Fira Code", weight="bold", size=30)
-update_panel_font = CTkFont(family="Fira Code", weight="bold", size=12)
-side_panel_font = CTkFont(family="Fira Code", weight="bold", size=14)
